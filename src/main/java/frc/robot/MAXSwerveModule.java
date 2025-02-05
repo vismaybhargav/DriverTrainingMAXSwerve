f87ae47fd5076e3f665b7905031b30a5975d8385
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-02-04 21:39:10 -0800

    (fix) add constants and MAXSwerveModule

--- src/main/java/frc/robot/MAXSwerveModule.java
+++ src/main/java/frc/robot/MAXSwerveModule.java
@@ -1,3 +1,5 @@
+package frc.robot;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
