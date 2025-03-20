632e043b6ef381b5811e86a811f8351ec2bc16f5
Tejas <tbhojo08@gmail.com>
2025-03-19 17:51:35 -0700

    (feat) raspberrypi files

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
@@ -194,8 +195,41 @@ public final class Constants {
 	}
 
 	public static final class VisionConstants {
+
+		public static final double MAX_SPEED_METERS_PER_SECOND = 0.15;
+		public static final double MAX_ANGULAR_SPEED_RADIANS_PER_SECOND = Math.PI * 0.1;
+	
+		//TODO: equate the margin to the probabilistic equation from HW.
+		public static final double X_MARGIN_TO_REEF = 0.02;
+		public static final double Y_MARGIN_TO_REEF = 0.02;
+		public static final double ROT_MARGIN_TO_REEF
+			= Math.toRadians(2); //TODO: change rot margin.
+		public static final double TAG_TARGET_DISTANCE = 0.2;
+	
+		public static final double N_180 = 180;
+	
+		public static final int AT_ARR_INC = 10;
+		public static final int AT_ARR_CAMERA_OFFSET = 1;
+		public static final int AT_ARR_TRANSLATION_OFFSET = 4;
+		public static final int AT_ARR_ROTATION_OFFSET = 7;
+	
 		public static final String REEF_CAM_NAME = "Reef_Camera";
 		public static final String SOURCE_CAM_NAME = "Source_Camera";
+	
+		public static final String APRIL_TAG_FIELD_LAYOUT_JSON =
+			Filesystem.getDeployDirectory() + "/apriltag/welded/apriltag.json";
+	
+		public static final int LOCALIZATION_TAG_NUM = 1;
+		public static final double LOCALIZATION_TRANSLATIONAL_THRESHOLD = 0.4;
+		public static final double LOCALIZATION_ANGLE_TOLERANCE = Math.toRadians(5);
+	
+		// TODO: tune constants based on field testing.
+		public static final double MIN_TAG_TARGET_DISTANCE_X = 0.2;
+		public static final double MAX_TAG_TARGET_DISTANCE_X = 2.0;
+		public static final double TAG_Y_SCALING_COEF = 0.6;
+		public static final double TAG_TARGET_THETA_RAD = Math.PI / 5;
+		public static final double TAG_Y_L_OFFSET_BASE = Constants.AutoConstants.REEF_Y_L_TAG_OFFSET * 2;
+		public static final double TAG_Y_R_OFFSET_BASE = Constants.AutoConstants.REEF_Y_R_TAG_OFFSET * 2;
 	}
 
 	public static final class OIConstants {
--- src/main/java/frc/robot/vision/RaspberryPi.java
+++ src/main/java/frc/robot/vision/RaspberryPi.java
@@ -1,111 +1,168 @@
 package frc.robot.vision;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.DoubleArrayTopic;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants;
 import frc.robot.Constants.VisionConstants;
 
-import java.util.ArrayList;
-import org.photonvision.PhotonCamera;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
-/** IO implementation for real robot using PhotonVision. */
+/**
+* This class is used to get the data from the Raspberry Pi.
+*
+* @author Jaseer Abdulla
+*/
 public class RaspberryPi {
-	private final PhotonCamera reefCamera;
-	private final PhotonCamera stationCamera;
+	private NetworkTable reefTable;
+	private NetworkTable sourceTable;
+	private DoubleArraySubscriber reefCamSubscriber;
+	private DoubleArraySubscriber sourceCamSubscriber;
+	private final String reefCamName;
+	private final String sourceCamName;
 
 	/**
-	 * Creates a new RaspberryPiPhoton connecting to real cameras.
-	 */
+	* Default constructor for the RaspberryPi class.
+	*/
 	public RaspberryPi() {
-		// Initialize cameras with their network names
-		reefCamera = new PhotonCamera(VisionConstants.REEF_CAM_NAME);
-		stationCamera = new PhotonCamera(VisionConstants.SOURCE_CAM_NAME);
+		reefTable = NetworkTableInstance.getDefault().getTable("reef_table");
+		DoubleArrayTopic reefCamTopic = reefTable.getDoubleArrayTopic("april_tag_data");
+		reefCamSubscriber = reefCamTopic.subscribe(new double[] {});
+
+		sourceTable = NetworkTableInstance.getDefault().getTable("source_table");
+		DoubleArrayTopic sourceCamTopic = sourceTable.getDoubleArrayTopic("april_tag_data");
+		sourceCamSubscriber = sourceCamTopic.subscribe(new double[] {});
+
+		reefCamName = VisionConstants.REEF_CAM_NAME;
+		sourceCamName = VisionConstants.SOURCE_CAM_NAME;
 	}
 
 	/**
-	 * Prints all raw apriltag data to console.
+	 * Prints the raw data for the april tags on the rpi.
 	 */
 	public void printRawData() {
-		for (AprilTag tag : getAprilTags()) {
-			System.out.println("AprilTag " + tag.getTagID() + " -> " + tag.getPose().toString());
-		}
+		double[] reefRawData = reefCamSubscriber.get();
+		double[] sourceRawData = sourceCamSubscriber.get();
+		System.out.println("Reef Raw Data: " + Arrays.toString(reefRawData));
+		System.out.println("Source Raw Data: " + Arrays.toString(sourceRawData));
 	}
 
 	/**
-	 * Returns a list of all april tags from reef and station camera.
-	 * @return all april tags
-	 */
+	* Returns a list of all AprilTags from all cameras.
+	* @return A list of visible AprilTags
+	*/
 	public ArrayList<AprilTag> getAprilTags() {
 		ArrayList<AprilTag> atList = new ArrayList<>();
-		atList.addAll(getReefAprilTags());
-		atList.addAll(getStationAprilTags());
+		atList.addAll(getAprilTagsSingleCam(reefCamSubscriber, reefCamName));
+		atList.addAll(getAprilTagsSingleCam(sourceCamSubscriber, sourceCamName));
 		return atList;
 	}
 
 	/**
-	 * Returns a list of all april tags from reef CV camera.
-	 * @return all visible reef april tags.
-	 */
-	public ArrayList<AprilTag> getReefAprilTags() {
+	* Returns a list of all AprilTags from one camera.
+	* @param camSub subscriber for the camera
+	* @param camName camera name
+	* @return A list of visible AprilTags
+	*/
+	public ArrayList<AprilTag> getAprilTagsSingleCam(DoubleArraySubscriber camSub, String camName) {
 		ArrayList<AprilTag> atList = new ArrayList<>();
+		double[] rawData = camSub.get();
 
-		var results = reefCamera.getLatestResult();
-		if (results.hasTargets()) {
-			System.out.println(results.getTargets().size());
-			for (PhotonTrackedTarget target : results.getTargets()) {
-				System.out.println(target.getArea());
-				AprilTag at = new AprilTag(
-					target.getFiducialId(),
-					reefCamera.getName(),
-					new Translation3d(), // camera vector, unused
+		if (rawData.length == 0) {
+			return atList;
+		}
+
+		for (
+			int i = 0;
+			i < rawData.length;
+			i += VisionConstants.AT_ARR_INC
+		) {
+			atList.add(
+				new AprilTag(
+					(int) rawData[i],
+					camName,
 					new Translation3d(
-						target.getBestCameraToTarget().getY(),
-						target.getBestCameraToTarget().getZ(),
-						target.getBestCameraToTarget().getX()
+						rawData[i + Constants.VisionConstants.AT_ARR_CAMERA_OFFSET],
+						rawData[i + VisionConstants.AT_ARR_CAMERA_OFFSET + 1],
+						rawData[i + VisionConstants.AT_ARR_CAMERA_OFFSET + 2]
+					),
+					new Translation3d(
+						rawData[i + VisionConstants.AT_ARR_TRANSLATION_OFFSET],
+						rawData[i + VisionConstants.AT_ARR_TRANSLATION_OFFSET + 1],
+						rawData[i + VisionConstants.AT_ARR_TRANSLATION_OFFSET + 2]
 					),
 					new Rotation3d(
-						target.getBestCameraToTarget().getRotation().getY(),
-						target.getBestCameraToTarget().getRotation().getZ(),
-						target.getBestCameraToTarget().getRotation().getX()
+						rawData[i + VisionConstants.AT_ARR_ROTATION_OFFSET],
+						rawData[i + VisionConstants.AT_ARR_ROTATION_OFFSET + 1],
+						rawData[i + VisionConstants.AT_ARR_ROTATION_OFFSET + 2]
 					)
-				);
-				atList.add(at);
-			}
+				)
+			);
 		}
 		return atList;
 	}
 
 	/**
-	 * Returns all april tags visible from Station CV Camera.
-	 * @return list of all april tags
+	 * Gets an April Tag from the list given a certain tag.
+	 * @param id id of the april tag
+	 * @return the april tag matching the id
+	 */
+	public AprilTag getAprilTagWithID(int id) {
+		return getAprilTags()
+			.stream()
+			.filter(tag -> tag.getTagID() == id)
+			.findFirst()
+			.orElse(null);
+	}
+
+	/**
+	 * Get all april tags reported from the station camera.
+	 * @return list of all apriltags
 	 */
 	public ArrayList<AprilTag> getStationAprilTags() {
-		ArrayList<AprilTag> atList = new ArrayList<>();
+		return getAprilTagsSingleCam(sourceCamSubscriber, VisionConstants.SOURCE_CAM_NAME);
+	}
 
-		var results = stationCamera.getLatestResult();
-		if (results.hasTargets()) {
-			for (PhotonTrackedTarget target : results.getTargets()) {
-				AprilTag at = new AprilTag(
-					target.getFiducialId(),
-					stationCamera.getName(),
-					new Translation3d(), // camera vector, unused
-					new Translation3d(
-						-target.getBestCameraToTarget().getY(),
-						-target.getBestCameraToTarget().getZ(),
-						-target.getBestCameraToTarget().getX()
-					),
-					new Rotation3d(
-						target.getBestCameraToTarget().getRotation().getY(),
-						target.getBestCameraToTarget().getRotation().getZ(),
-						target.getBestCameraToTarget().getRotation().getX()
-					)
-				);
-				// if (at.getPose().getTranslation().getNorm()
-					// < VisionConstants.MAX_TAG_TARGET_DISTANCE_X) {
-				atList.add(at);
-				// }
-			}
+	/**
+	 * Get all april tags reported from the reef camera.
+	 * @return list of all april tags
+	 */
+	public ArrayList<AprilTag> getReefAprilTags() {
+		return getAprilTagsSingleCam(reefCamSubscriber, VisionConstants.REEF_CAM_NAME);
+	}
+
+	/**
+	 * Checks if any AprilTags are in view.
+	 * @return A boolean representing if any tags are in view
+	 */
+	public boolean canSeeTags() {
+		return getAprilTags().size() != 0;
+	}
+
+	/**
+	 * Returns the closest AprilTag from any camera.
+	 * @return The closest AprilTag object. If none are in view, returns null.
+	 */
+	public AprilTag getClosestTag() {
+		ArrayList<AprilTag> atlist = getAprilTags();
+		if (getAprilTags().size() == 0) {
+			return null;
 		}
-		return atList;
+		return Collections.max(atlist);
+	}
+
+	/**
+	 * Updates the raspberry pi's values given the current robot pose.
+	 * Not used for teleop functionality.
+	 * @param pose
+	 */
+	public void update(Pose2d pose) {
+		// pass
 	}
 }
