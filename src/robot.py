9ca4a3be883213ea6e64a42fffd85ca0dbe0f1e3
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-03-24 09:59:41 -0700

    (feat) init robotpy

---
+++ src/robot.py
@@ -0,0 +1,13 @@
+# TODO: insert robot code here
+
+import wpilib
+import wpilib.drive
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self) -> None:
+        """
+        This function is called on program startup.
+        """
+        
+        self.
+
